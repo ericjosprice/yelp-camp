717d0a205ecddb022dbd04a60aa23994ce12063c
ericjosprice <epriceutexas@gmail.com>
2019-07-15 14:15:14 -0500

    catch all route to serve static react files

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const seeds = require('./seeds');
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -20,6 +20,11 @@ if (process.env.NODE_ENV === "production") {
 
 // Add routes, both API and view
 app.use(routes);
+
+//does heroku redirect requests to the react router dom with this code?
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+});
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/yelpcamp", {useNewUrlParser: true});
