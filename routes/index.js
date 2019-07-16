b3ec0fd70953375244cab4cfb902d3f90e3dfa51
ericjosprice <epriceutexas@gmail.com>
2019-07-15 19:11:14 -0500

    catch all route for deployment added to routes index

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,16 @@ const apiRoutes = require("./api");
 // API Routes
 router.use("/api", apiRoutes);
 
-router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// router.use(function(req, res) {
+//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 
 
 // Send every request to the React app
 // Define any API routes before this runs
-// router.use("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+router.use("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 module.exports = router;
