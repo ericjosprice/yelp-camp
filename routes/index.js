1f30c4b6f8cc42f51048b16e177fdcaef9c0e55b
ericjosprice <epriceutexas@gmail.com>
2019-07-15 19:24:17 -0500

    adjusted directory path for catch all route response

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.use("/api", apiRoutes);
 // Send every request to the React app
 // Define any API routes before this runs
 router.use("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
